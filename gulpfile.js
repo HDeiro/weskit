f9e30f8c98f1454a492aafed6225879004422ec9
Hugo <hugodeiro@gmail.com>
2019-03-04 11:43:37 -0300

    Refactoring JS bundle generator

--- gulpfile.js
+++ gulpfile.js
@@ -65,51 +65,29 @@ const tasks = {
 // 'JavaScripts' instead of 'js'
 //####################################
 
-const project_dist = 'www';
-const project_src = 'app';
+const path_build = 'www';
+const path_source = 'app';
 const paths = {
-    // JavaScripts
-    scripts: {
-        dest: `${project_dist}/js`,
-        origin: {
-            internal_root: `${project_src}/scripts`,
-            internal: [
-                `${project_src}/scripts/script.js`
-            ],
-            external: [
-            ]
-        }
-    },
-    // Styles (SASS/CSS)
-    styles: {
-        dest: `${project_dist}/css`,
-        origin: {
-            // SASS files
-            internal: [
-                `${project_src}/styles/style.{scss,sass}`
-            ],
-            // CSS files
-            external: [
+    js: {
+        "internal-critical": {
+            "destination": `${path_build}/js`,
+            "bundle": []
+        },
+        "internal": {
+            "destination": `${path_build}/js`,
+            "bundle": []
+        },
+        "external-critical": {
+            "destination": `${path_build}/js`,
+            "bundle": [
+                'www/a/*.js'
             ]
         },
-        origin_root: `${project_src}/styles`
-    },
-    // Views
-    views: {
-        dest: project_dist,
-        origin: `${project_src}/**/*.{html,php}`
-    },
-    // Images to be minified
-    images: {
-        dest: `${project_dist}/images`,
-        origin: `${project_src}/images/**/*`,
-        sprites_origins: `${project_src}/images/sprites/**/*`,
-    },
-    // Folders and files to be cleaned after development
-    to_be_cleanded: [
-        project_dist,
-        'node_modules'
-    ]
+        "external": {
+            "destination": `${path_build}/js`,
+            "bundle": []
+        }
+    }
 }
 
 
@@ -121,182 +99,16 @@ const paths = {
 // replaces and a lot of other possibilities
 //####################################
 
-gulp.task(tasks.css, () => {
-    const cssStream = gulp.src(paths.styles.origin.external);
-    const sassStream = gulp.src(paths.styles.origin.internal)
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .on('error', sass.logError);
-
-    return estream.merge(cssStream, sassStream)
-        .pipe(sourcemaps.init())
-        .pipe(concat('style.css'))
-        .pipe(autoprefixer())
-        .pipe(cssmin())
-        .pipe(sourcemaps.write('.'))
-        .pipe(bsync.stream({match: '**/*.css'}))
-        .pipe(gulp.dest(paths.styles.dest))
-        .on('end', () => console.log(`SASS files has been concatenated and minifed`));
-});
-
 gulp.task(tasks.js_concat, () => {
-    return gulp.src(paths.scripts.origin.external.concat(paths.scripts.origin.internal))
-        .pipe(sourcemaps.init())
-        .pipe(concat('script.js'))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(bsync.stream({match: '**/*.js'}))
-        .pipe(gulp.dest(paths.scripts.dest))
-        .on('end', () => console.log(`Listed origin and external JavaScript files has been concatenated`));
-});
-
-gulp.task(tasks.js_uglify, cb => {
-    runsequence(tasks.js_concat, () => {
-        const options = {
-            preserveComments: 'license',
-            compress: {
-                drop_console: true
-            }
-        };
-        
-        pump([
-            gulp.src(paths.scripts.dest + '/script.js'),
-            uglify(options),
-            rename('script.min.js'),
-            gulp.dest(paths.scripts.dest)
-        ], cb)
-        .on('end', () => console.log(`${paths.scripts.dest}/script.js has been minified`));
-    });
-});
-
-gulp.task(tasks.html, () => {
-    return gulp.src(paths.views.origin)
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true,
-            ignoreCustomComments: [
-                /build:[a-zA-Z]{1,}/,
-                /endbuild/,
-            ]
-        }))
-        .pipe(bsync.stream({match: '**/*.{html,php}'}))
-        .pipe(gulp.dest(paths.views.dest))
-        .on('end', () => console.log(`${paths.views.origin} has been minified`));
-});
-
-gulp.task(tasks.watch, () => {
-    gulp.watch(paths.scripts.origin.internal_root + '/**/*.js', [tasks.js_concat]);
-    gulp.watch(paths.styles.origin_root + '/**/*.{sass,scss}', [tasks.css]);
-    gulp.watch(paths.views.origin, [tasks.html]);
-});
-
-gulp.task(tasks.server, () => {
-    bsync.init({
-        server: {
-            baseDir: `${project_dist}/`
-        }
-    });
-
-    gulp.watch(paths.scripts.origin.internal_root + '/**/*.js', [tasks.js_concat]).on('change', bsync.reload);
-    gulp.watch(paths.styles.origin_root + '/**/*.scss', [tasks.css]).on('change', bsync.reload);
-    gulp.watch(paths.views.origin, [tasks.html]).on('change', bsync.reload);
-});
-
-gulp.task(tasks.images, () => {
-    return gulp.src(paths.images.origin)
-        .pipe(imagemin([
-            imagemin.gifsicle({
-                interlaced: true,
-                optimizationLevel: 1 // Minimum 1 and Maximum 3
-            }),
-            imagemin.jpegtran({
-                progressive: true
-            }),
-            imagemin.optipng({
-                optimizationLevel: 5 // Minimum 0 and Maximum 7
-            }),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true}
-                ]
-            })
-        ]))
-        .pipe(gulp.dest(paths.images.dest))
-        .on('end', () => console.log(`${paths.images.dest} has now optimizated images from ${paths.images.origin}`));
-});
-
-gulp.task(tasks.sprites, () => {
-    return sprity.src({
-        src: `${paths.images.sprites_origins}.{png,jpg}`,
-        style: `./sprites.scss`,
-        processor: 'sass'
-    }).pipe(gulpif(
-        '*.png',
-        gulp.dest(paths.images.dest),
-        gulp.dest(paths.styles.origin_root)
-    )).on('end', () => {
-        console.log(`${paths.images.dest}/sprite.png has been created`);
-        console.log(`${paths.styles.origin_root}/sprites.scss has been created`);
-    })
-});
-
-gulp.task(tasks.html_replace, () => {
-    runsequence(tasks.html, () => {
-        let css = `${paths.styles.dest}/style.css`;
-        let js = `${paths.scripts.dest}/script.js`;
-
-        // Use gulp htmlreplace --xcss
-        if(yargs.xcss) {
-           // Take the content of CSS file and rip off all the "../" 
-           // references to refer from the index.html
-            css = fs.readFileSync(css, 'utf8').split('../').join('');
-            css = `<style>${css}</style>`;
-            console.log('Exchanged the CSS reference for the stylesheet content');
-        }
-
-        // Use gulp htmlreplace --xjs
-        //
-        // You can use like: gulp htmlreplace --xjs --xcss
-        // to apply css and javascript replacement
-        if(yargs.xjs) {
-            //Take the content of JavaScript concatenated file and put it direct on body of index.html
-            js = fs.readFileSync(js, 'utf8');
-            js = `<script>${js}</script>`;
-            console.log('Exchanged the JS reference for the script content');
-        }
-
-        // It takes all html/php files and apply the html
-        // replacement. It's important to say that all the
-        // files will have the same reference/content for
-        // CSS or JavaScript.
-        gulp.src(`${project_dist}/**/*.{html,php}`)
-            .pipe(htmlreplace({
-                'js': js,
-                'css': css
-            }))
-            .pipe(gulp.dest(`${project_dist}/`))
-            .on('end', () => {
-                console.log('HTML replacement is done');
-            });
-    });
-});
-
-gulp.task(tasks.production, () => {
-    runsequence(
-        tasks.images,
-        tasks.js_uglify, 
-        tasks.css, 
-        tasks.html_replace, 
-        () => {
-            console.log('The production task has finished.');
-        }
-    );
-});
-
-gulp.task(tasks.clean, () => {
-    return gulp.src(paths.to_be_cleanded)
-        .pipe(clean({ force: true }));
+    Object.keys(paths.js)
+        .filter(bundle => paths.js[bundle].bundle.length)
+        .filter(bundle => yargs.bundle ? yargs.bundle == bundle : true)
+        .forEach(bundle => {
+            gulp.src(paths.js[bundle].bundle)
+                .pipe(concat(`${bundle}.js`))
+                .pipe(babel({presets: ['es2015']}))
+                .pipe(sourcemaps.write('.'))
+                .pipe(bsync.stream({match: '**/*.js'}))
+                .pipe(gulp.dest(`${paths.js[bundle].destination}`))
+        });
 });
