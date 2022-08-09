1b4d72cba8d3d2b480db2377830a95243edfacf8
Hugo Deiró <hugodeiro@gmail.com>
2022-08-09 10:59:02 -0300

    Cleanup before v3 changes

--- app/pwa-cache-service-worker.js
+++ app/pwa-cache-service-worker.js
@@ -9,14 +9,22 @@ const cachable = [
 ];
 
 // On SW installation
-self.addEventListener('install', event => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(cachable).then(() => self.skipWaiting()))));
+self.addEventListener('install', event => event
+	.waitUntil(caches
+		.open(cacheName)
+		.then(cache => cache
+			.addAll(cachable)
+			.then(() => self.skipWaiting())
+		)
+	)
+);
 
 // On SW Activation
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 
 // On cache fetching
-self.addEventListener('fetch', event => event.respondWith(
-    caches.open(cacheName)
-        .then(cache => cache.match(event.request, {ignoreSearch: true}))
-        .then(response => response || fetch(event.request))
+self.addEventListener('fetch', event => event.respondWith(caches
+	.open(cacheName)
+    .then(cache => cache.match(event.request, {ignoreSearch: true}))
+    .then(response => response || fetch(event.request))
 ));
